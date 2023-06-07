d8c458b50698988f268d988fa88ef4ea3ac3926e
joniqfzhang <qfzhang18@hotmail.com>
2023-06-08 00:26:21 +1000

    Update settings.py
    update files

--- todosite/settings.py
+++ todosite/settings.py
@@ -137,9 +137,8 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'qfzhang18@gamil.com'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'qfzhang180@gmail.com'
-EMAIL_HOST_USER = 'qfzhang18@gmail.com'
-EMAIL_HOST_PASSWORD = 'bhoypqspgbzlfshj'
+EMAIL_FROM = 'your@gmail.com'
+EMAIL_HOST_USER = 'your@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
